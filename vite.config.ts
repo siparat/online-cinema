3431faeaf918b265e9edbad2d8c053b12bcfb5ec
siparat <akimov1712@yandex.ru>
2025-05-25 16:40:36 +0300

    feat: append svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-	alias: {
-		"@": resolve(__dirname, './src')
+	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src')
+		}
 	}
-  }
-})
+});
